5f6ae9340df069a5175342cd3b862535446f91af
HamdiTliba <hamditliba.go@gmail.com>
2024-02-24 15:39:55 +0100

    add mobile menu

--- componets/navbar/links/Links.jsx
+++ componets/navbar/links/Links.jsx
@@ -1,6 +1,9 @@
+"use client";
+import { useState } from "react";
 import styles from "./link.module.css";
 import NavLinks from "./navLinks/NavLinks";
 const Links = () => {
+  const [open, setOpen] = useState(false);
   const links = [
     {
       title: "Home",
@@ -19,11 +22,44 @@ const Links = () => {
       page: "/contact",
     },
   ];
+  const session = true;
+  const isAdmin = true;
   return (
-    <div className={styles.link}>
-      {links.map((link) => (
-        <NavLinks item={link} key={link.title} />
-      ))}
+    <div className="link-container">
+      <div className={styles.link}>
+        {links.map((link) => (
+          <NavLinks item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLinks item={{ title: "Admin", page: "/admin" }} />}
+            <button>Logout</button>
+          </>
+        ) : (
+          <button>Login </button>
+        )}
+      </div>
+
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLinks item={link} key={link.title} />
+          ))}
+          {session ? (
+            <>
+              {isAdmin && (
+                <NavLinks item={{ title: "Admin", page: "/admin" }} />
+              )}
+              <button>Logout</button>
+            </>
+          ) : (
+            <button>Login </button>
+          )}
+        </div>
+      )}
+      <button className={styles.btnMenu} onClick={() => setOpen((pre) => !pre)}>
+        Menu
+      </button>
     </div>
   );
 };
